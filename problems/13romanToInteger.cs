cac4161935f8e716492541c07f085689516a672d
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-05 12:02:05 +0400

    13. Roman to Integer

---
+++ problems/13romanToInteger.cs
@@ -0,0 +1,33 @@
+﻿namespace LeedCode_Problems.problems;
+
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> romaNumber = new Dictionary<char, int>{
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        int sum = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            int value = romaNumber[s[i]];
+
+            if (i + 1 < s.Length && romaNumber[s[i + 1]] > value)
+            {
+                sum -= value;
+            }
+            else
+            {
+                sum = sum + value;
+
+            }
+        }
+        return sum;
+    }
+}
