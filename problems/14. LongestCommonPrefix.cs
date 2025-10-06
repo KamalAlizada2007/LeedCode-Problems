5517539b873fbad2926ad35b95e8a00889fa98c6
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-06 23:23:44 +0400

    14. Longest Common Prefix

---
+++ problems/14. LongestCommonPrefix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems
+{
+	public class Solution14
+
+	{
+		public string LongestCommonPrefix(string[] words)
+		{
+			if (words.Length == 0) return "";
+
+			string prefix = "";
+			int minLength = words.Min(w => w.Length);
+
+			for (int i = 0; i < minLength; i++)
+			{
+				char current = words[0][i];
+
+				foreach (var word in words)
+				{
+					if (word[i] != current)
+						return prefix;
+				}
+
+				prefix += current;
+			}
+
+			return prefix;
+		}
+	}
+
+}
