a505395eb8572582daa45bc40585d6458d0b7c54
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-07 22:48:39 +0400

    20. Valid Parentheses

---
+++ problems/20.ValidParentheses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution20
+{
+    public bool IsValid(string s)
+    {
+
+        if (s.Length % 2 != 0)
+            return false;
+
+        while (s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
+        {
+
+            s = s.Replace("()", "");
+            s = s.Replace("[]", "");
+            s = s.Replace("{}", "");
+        }
+
+
+        return s.Length == 0;
+    }
+}
+
