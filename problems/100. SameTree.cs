e5a49018909cb9d770757a73f25e10acb331afda
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-09 21:21:37 +0400

    26. Remove Duplicates from Sorted Array

---
+++ problems/100. SameTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+
+public class Solution100
+{
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null)
+            return false;
+
+        return (p.val == q.val) &&
+        IsSameTree(p.left, q.left) &&
+        IsSameTree(p.right, q.right);
+
+
+
+
+
+
+
+    }
+}
+
---
+++ problems/26. RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,25 @@
+﻿namespace LeedCode_Problems.problems
+{
+	public class Solution26
+	{
+		public int RemoveDuplicates(int[] nums)
+		{
+
+			Array.Sort(nums);
+
+			int result = 1;
+			for (int i = 1; i < nums.Length; i++)
+			{
+				if (nums[i] != nums[i - 1])
+				{
+					nums[result] = nums[i];
+					result++;
+				}
+			}
+			return result;
+
+
+
+		}
+	}
+}
