08af80986d7ec9b943805a1e01f066bd85d6838b
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-11 22:28:32 +0400

    58. Length of Last Word

---
+++ problems/27. RemoveElement.cs
@@ -0,0 +1,23 @@
+﻿namespace LeedCode_Problems.problems
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int  k = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+
+            }
+            return k;
+        }
+
+        
+    }
+}
---
+++ problems/58.LengthofLastWord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution58
+{
+    public int LengthOfLastWord(string s)
+    {
+
+        string[] words = s.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string lastWord = words[^1];
+        return lastWord.Length;
+
+    }
+}
