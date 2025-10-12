b60e693a945f35dba1c887624c0940a1dc2dca8a
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-12 23:20:46 +0400

    35. Search Insert Position

---
+++ problems/35.SearchInsertPosition.cs
@@ -0,0 +1,34 @@
+﻿namespace LeedCode_Problems.problems;
+public class Solution35
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+
+        if (nums.Contains(target))
+        {
+            return Array.IndexOf(nums, target);
+        }
+
+        int[] newArray = new int[nums.Length + 1];
+
+        int i = 0, j = 0;
+
+        while (i < nums.Length && nums[i] < target)
+        {
+            newArray[j++] = nums[i++];
+
+        }
+
+        newArray[j++] = target;
+
+        while (j < nums.Length)
+        {
+            newArray[j++] = nums[i++];
+        }
+
+        return Array.IndexOf(newArray, target);
+
+
+
+    }
+}
