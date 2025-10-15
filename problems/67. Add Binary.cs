81e560dba1b9bfc88d57b8c058f5e65b1cc152f4
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-15 22:06:38 +0400

    67. Add Binary

---
+++ problems/67. Add Binary.cs
@@ -0,0 +1,32 @@
+﻿namespace LeedCode_Problems.problems;
+public class Solution67
+{
+    public string AddBinary(string a, string b)
+    {
+
+
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+        List<char> result = new List<char>();
+
+        while (i >= 0 || j >= 0 || carry > 0)
+        {
+            int bitA = (i >= 0) ? a[i] - '0' : 0;
+            int bitB = (j >= 0) ? b[j] - '0' : 0;
+
+            int sum = bitA + bitB + carry;
+            result.Add((char)((sum % 2) + '0'));
+            carry = sum / 2;
+
+            i--;
+            j--;
+        }
+
+        result.Reverse();
+        string res = new string(result.ToArray());
+        return res;
+
+    }
+}
+
