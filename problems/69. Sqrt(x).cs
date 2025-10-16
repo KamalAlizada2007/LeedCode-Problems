51ed0bb955ba973292d8f13a0c8d1419b3a17b6b
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-16 19:38:33 +0400

    69. Sqrt(x)

---
+++ problems/69. Sqrt(x).cs
@@ -0,0 +1,14 @@
+﻿namespace LeedCode_Problems.problems;
+public class Solution69
+{
+    public int MySqrt(int x)
+    {
+        double result = Math.Sqrt(x);
+        int quation = (int)result;
+
+
+
+        return quation;
+
+    }
+}
