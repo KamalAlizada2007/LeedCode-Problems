a7fe319a1d854eb0de5c2e18f35b2a690845ec0d
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-16 20:52:58 +0400

    70. Climbing Stairs

---
+++ problems/70. ClimbingStairs.cs
@@ -0,0 +1,34 @@
+﻿namespace LeedCode_Problems.problems;
+public class Solution70
+{
+    public int ClimbStairs(int n)
+    {
+
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        int sum = 0;
+        List<int> list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            sum = list[i] + list[i + 1];
+            list.Add(sum);
+
+
+        }
+
+        int result = list.Last();
+        return result;
+
+
+
+
+
+
+    }
+}
