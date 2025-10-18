6365320c162aa97619ede94a98270d186eb5c64f
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-18 13:22:51 +0400

    83. Remove Duplicates from Sorted List

--- problems/83.RemoveDuplicatesfromSortedList.cs
+++ problems/83.RemoveDuplicatesfromSortedList.cs
@@ -1,30 +1,28 @@
 ﻿namespace LeedCode_Problems.problems;
-
-public class Solution
+public class Solution83
 {
     public ListNode DeleteDuplicates(ListNode head)
     {
 
         List<int> duplicates = new List<int>();
-        for (var cur = head; cur != null; cur = cur.next)
+        for (var cur = head; cur != null; cur = cur.Next)
         {
-            duplicates.Add(cur.val);
+            duplicates.Add(cur.Value);
         }
 
         duplicates.Sort();
 
-        for (int i = 0; i < duplicates.Count - 1; i++)
+        for (int i = 0; i < duplicates.Count; i++)
         {
-            if (duplicates[i] == duplicates[i + 1])
+            if (duplicates.Contains(duplicates[i]))
             {
 
-                duplicates.RemoveAt(i + 1);
-                i--;
+                duplicates.Remove(duplicates[i]);
 
             }
         }
 
-        ListNode endResult = new ListNode(0);
+        ListNode endResult = new ListNode(1);
         var newCur = endResult;
         foreach (var v in duplicates)
         {
@@ -32,7 +30,7 @@ public class Solution
         }
 
 
-        return endResult.next;
+        return endResult;
 
 
     }
