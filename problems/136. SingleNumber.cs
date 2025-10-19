2e13fd98975e219f8fed3e09598c137f20261d09
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-19 15:14:39 +0400

    136. Single Number

---
+++ problems/136. SingleNumber.cs
@@ -0,0 +1,23 @@
+﻿namespace LeedCode_Problems.problems;
+public class Solution136
+
+{
+    public int SingleNumber(int[] nums)
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (result.Contains(nums[i]))
+            {
+                result.Remove(nums[i]);
+            }
+            else
+            {
+                result.Add(nums[i]);
+            }
+        }
+        return result[0];
+
+    }
+}
+
