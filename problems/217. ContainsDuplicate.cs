e59cdc75d50d6d0e659fb535a6f0e98101df7307
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-21 23:45:22 +0400

    217. Contains Duplicate

---
+++ problems/217. ContainsDuplicate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+
+public class Solution217
+
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> seen = new HashSet<int>();
+
+        foreach (int num in nums)
+        {
+            if (seen.Contains(num))
+                return true;
+
+            seen.Add(num);
+        }
+
+        return false;
+    }
+}
+
