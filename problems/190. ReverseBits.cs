f38627c8dcf5614d8cbb9ca1b687e2f2b72f2bee
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-23 22:27:17 +0400

    190. Reverse Bits

---
+++ problems/190. ReverseBits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution190
+{
+    public int ReverseBits(int n)
+    {
+        uint num = (uint)n;
+        uint result = 0;
+
+        for (int i = 0; i < 32; i++)
+        {
+            result <<= 1;
+            result |= (num & 1);
+            num >>= 1;
+        }
+
+        return (int)result;
+    }
+}
+
