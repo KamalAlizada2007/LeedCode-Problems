424a6c0201024ac44dd2b386203d21bf36a1bbad
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-25 23:55:18 +0400

    231. Power of Two

---
+++ problems/231. PowerofTwo.cs
@@ -0,0 +1,13 @@
+﻿namespace LeedCode_Problems.problems;
+public class Solution231
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        if (n <= 0) return false;
+        while (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        return n == 1;
+    }
+}
