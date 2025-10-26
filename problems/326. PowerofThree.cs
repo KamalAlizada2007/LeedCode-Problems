322b18455025a291f51e570f495df2815c97b6a4
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-26 23:46:47 +0400

    326. Power of Three

---
+++ problems/326. PowerofThree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution326
+{
+    public bool IsPowerOfThree(int n)
+    {
+        if (n <= 0) return false;
+        while (n % 3 == 0)
+        {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
+
