1298dd7caaa84a84b1cb1cb3b84ffef02accacf9
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-27 18:12:28 +0400

    342. Power of Four

---
+++ problems/342. PowerofFour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution342
+{
+    public bool IsPowerOfFour(int n)
+    {
+        if (n <= 0) { return false; }
+        if (n == 1) return true;
+        double a = n;
+        while (a >= 4)
+        {
+            a = a / 4;
+            if (a == 1)
+            {
+                return true;
+            }
+            else if (a > 1 && a < 4)
+            {
+                return false;
+            }
+        }
+        return false;
+
+    }
+}
