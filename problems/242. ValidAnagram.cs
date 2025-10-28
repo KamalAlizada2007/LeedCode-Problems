b50407e0dd4d2938d2c0c6b7fc752d780fcf5072
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-28 22:40:01 +0400

    242. Valid Anagram

---
+++ problems/242. ValidAnagram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution242
+{
+    public bool IsAnagram(string s, string t)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            for (int j = 0; j < t.Length; j++)
+            {
+                if (s[i] == t[j])
+                {
+                    s = s.Remove(i, 1);
+                    t= t.Remove(j, 1);
+
+                    i-= 1;
+                    break;
+
+                   
+                }
+            }
+        }
+        return s.Length == 0 && t.Length == 0;
+
+
+
+    }
+}
