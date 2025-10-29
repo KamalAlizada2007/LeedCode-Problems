7da83d5a4437bd455b77905e1a947914ed84c676
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-29 22:13:57 +0400

    169. Majority Element

---
+++ problems/169. MajorityElement.cs
@@ -0,0 +1,23 @@
+﻿namespace LeedCode_Problems.problems;
+public class Solution169
+{
+    public int MajorityElement(int[] nums)
+    {
+        int candidate = 0;
+        int count = 0;
+
+        foreach (int num in nums)
+        {
+            if (count == 0)
+                candidate = num;
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        int freq = nums.Count(x => x == candidate);
+        if (freq > nums.Length / 2)
+            return candidate;
+
+        return -1;
+    }
+}
