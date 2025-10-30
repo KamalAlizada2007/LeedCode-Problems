4c8bf8a10c097a007efddd0e98ce73f48a4ecaa4
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-30 23:02:00 +0400

    283. Move Zeroes

---
+++ problems/283. MoveZeroes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution283
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int nonZeroIndex = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[nonZeroIndex] = nums[i];
+                nonZeroIndex++;
+
+            }
+        }
+
+        for (int i = nonZeroIndex; i < nums.Length; i++)
+        {
+            nums[i] = 0;
+        }
+
+    }
+}
