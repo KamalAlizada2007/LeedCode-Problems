022e101e4303337984baf217bb7407d9d066af20
Kamal Alizada <alizadakamal07@gmail.com>
2025-10-31 23:05:27 +0400

    389. Find the Difference

---
+++ problems/389Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace LeedCode_Problems.problems;
+
+public class Solution389
+{
+    public char FindTheDifference(string s, string t)
+    {
+        foreach (char c in s)
+        {
+            int index = t.IndexOf(c);
+            if (index != -1)
+                t = t.Remove(index, 1);
+        }
+
+        return t[0];
+    }
+}
+
+
+
+
