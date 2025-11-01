8fbef4ae33e044522684929a6678df7f6c2c5c2a
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-01 23:04:06 +0400

    258. Add Digits

---
+++ problems/258. AddDigits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution258
+{
+    public int AddDigits(int num)
+    {
+        while (num >= 10)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
+
